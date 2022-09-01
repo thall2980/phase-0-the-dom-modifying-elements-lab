e340089d019818ecfe05ffc6df2f0bc4cd6f4e33
Tyler Hall <thall2980@gmail.com>
2022-09-01 11:42:56 -0400

    commit tree 1b4a1814a8397696208d35b5abef32e17c7b3444

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code here!
+
+function removeMainId(element) {
+    document.querySelector(element).remove()
+}
+
+removeMainId('main')
+
+
+let newHeader = document.createElement('h1')
+newHeader.id = "victory"
+newHeader.textContent = "Tyler is the champion"
+
+
+document.body.append(newHeader)
+
+
